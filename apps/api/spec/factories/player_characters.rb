1d24095df5ec7449e36c61a6aed171c51adce82e
Nick Hogan <nhogan411@gmail.com>
2026-04-04 13:29:15 -0500

    feat: add XP/level/award_xp! to PlayerCharacter

--- apps/api/spec/factories/player_characters.rb
+++ apps/api/spec/factories/player_characters.rb
@@ -4,5 +4,8 @@ FactoryBot.define do
     sequence(:name) { |n| PlayerCharacter::AVAILABLE_NAMES[(n - 1) % PlayerCharacter::AVAILABLE_NAMES.length] }
     archetype { 'warrior' }
     locked { false }
+    xp { 0 }
+    level { 1 }
+    max_hp { 16 }
   end
 end
