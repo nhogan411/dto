8a3e73563f44008c5f69c9d33c1bf0bcec26e82a
Nick Hogan <nhogan411@gmail.com>
2026-04-04 20:12:42 -0500

    refactor: extract serialize_game/serialize_character to GameSerializable concern

--- apps/api/app/controllers/application_controller.rb
+++ apps/api/app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
   include JwtAuthenticatable
   include ErrorRenderable
+  include GameSerializable
 end
--- apps/api/app/controllers/games_controller.rb
+++ apps/api/app/controllers/games_controller.rb
@@ -274,42 +274,6 @@ class GamesController < ApplicationController
     }
   end
 
-   def serialize_game(game)
-      {
-        id: game.id,
-        challenger_id: game.challenger_id,
-        challenged_id: game.challenged_id,
-        challenger_username: game.challenger.username,
-        challenged_username: game.challenged.username,
-        status: game.status,
-        board_config: serialize_board_config(game.board_config),
-        current_turn_user_id: game.current_turn_user_id,
-        winner_id: game.winner_id,
-         characters: game.game_characters.order(:id).map { |character| serialize_character(character) }
-      }
-    end
-
-   def serialize_board_config(board_config)
-     config = board_config.with_indifferent_access
-
-     {
-       tiles: config[:tiles]
-     }
-   end
-
-  def serialize_character(character)
-    {
-      id: character.id,
-      user_id: character.user_id,
-      position: character.position,
-      facing_tile: character.facing_tile,
-      current_hp: character.current_hp,
-      max_hp: character.max_hp,
-      is_defending: character.is_defending,
-      stats: character.stats
-    }
-  end
-
   def serialize_replay_action(action)
     {
       id: action.id,
